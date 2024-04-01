2d09cca8af18c1ad8282c53e67665e43eb9aa1ec
CodeProSpecialist <droark28@hotmail.com>
2024-03-31 19:27:40 -0500

    updates

--- stock_scanner.py
+++ stock_scanner.py
@@ -11,68 +11,77 @@ def get_price_data(symbol, start_date, end_date):
     data = yf.download(symbol, start=start_date, end=end_date)
     return data
 
-def calculate_indicators(close_prices):
+def calculate_indicators(data):
+    close_prices = data[:, 4]
     rsi = RSI(close_prices, timeperiod=14)
     macd, macd_signal, _ = MACD(close_prices, fastperiod=12, slowperiod=26, signalperiod=9)
     return rsi, macd, macd_signal
 
+def get_opening_price(symbol):
+    stock_data = yf.Ticker(symbol)
+    return round(stock_data.history(period="1d")["Open"].iloc[0], 4)
+
+def get_current_price(symbol):
+    stock_data = yf.Ticker(symbol)
+    return round(stock_data.history(period='1d')['Close'].iloc[0], 4)
+
 def analyze_stock(symbol):
-    end_date = datetime.now(pytz.timezone('US/Eastern')).strftime('%Y-%m-%d')
-    start_date_3_months_ago = (datetime.now(pytz.timezone('US/Eastern')) - timedelta(days=90)).strftime('%Y-%m-%d')
+    end_date = datetime.today().strftime('%Y-%m-%d')
+    start_date_3_months_ago = (datetime.today() - timedelta(days=90)).strftime('%Y-%m-%d')
 
-    price_data = get_price_data(symbol, start_date_3_months_ago, end_date)
+    price_data = get_price_data(symbol, start_date_3_months_ago, end_date).values
 
-    if price_data.empty:
-        print(f"No price data available for {symbol}")
-        return False, 0, 0, 0, 0, 0, 0, 0
+    current_close_price = price_data[-1, 3]
+    current_open_price = get_opening_price(symbol)
+    current_price = get_current_price(symbol)  # Get today's current price
+    current_volume = price_data[-1, 5]
+    average_volume = np.mean(price_data[:, 5])
 
-    close_prices = price_data['Close'].values
-    average_volume = np.mean(price_data['Volume'].values)
-
-    current_close_price = close_prices[-1]
-    current_open_price = price_data['Open'].iloc[-1]
-    current_price = price_data['Close'].iloc[-1]  # Get today's current price
-    current_volume = price_data['Volume'].iloc[-1]  # Get today's current volume
-    rsi, macd, macd_signal = calculate_indicators(close_prices)
-
-    print(f"\nAnalysis for {symbol}:")
-    print(f"Yesterday's Close Price: {current_close_price:.2f}")
-    print(f"Open Price for Today: {current_open_price:.2f}")
-    print(f"Current Price: {current_price:.2f}")  # Print today's current price
-    print(f"Current Volume: {current_volume:.2f}")
-    print(f"Average Volume: {average_volume:.2f}")
-    print(f"RSI: {rsi}")
-    print(f"MACD: {macd}")
+    rsi, macd, macd_signal = calculate_indicators(price_data)
 
     if (rsi[-1] > 58) and \
             (current_volume >= 0.25 * average_volume) and \
             (current_price > current_open_price) and \
             (current_price > current_close_price):
-        with open('buy_signals.txt', 'a') as file:
-            file.write(f"{symbol}\n")
         return True, round(current_close_price, 2), round(current_open_price, 2), round(current_price, 2), current_volume, average_volume, round(rsi[-1], 2), round(macd[-1], 2)
     else:
         return False, round(current_close_price, 2), round(current_open_price, 2), round(current_price, 2), current_volume, average_volume, round(rsi[-1], 2), round(macd[-1], 2)
 
+def get_previous_weekday(date):
+    while date.weekday() > 4:  # Monday is 0, Friday is 4
+        date -= timedelta(days=1)
+    return date
+
 
 def plot_stock_data(symbol):
     try:
-        end = datetime.now(pytz.timezone('US/Eastern'))
-        end = end - timedelta(hours=end.hour, minutes=end.minute, seconds=end.second, microseconds=end.microsecond)
-        start = end - timedelta(days=365)
-        data = get_price_data(symbol, start, end)
+        end = get_previous_weekday(datetime.today())
+        start = get_previous_weekday(end - timedelta(days=365))
+        data = yf.download(symbol, start, end)
 
-        plt.clear()
-        plt.plot(data.index, data['Close'], label='Close Price')
-        plt.plot(data.index, data['Open'], label='Open Price')
+        plt.clear_data()
+
+        # Plotting candlesticks manually
+        for i, (_, row) in enumerate(data.iterrows()):
+            plt.plot([i, i], [row['High'], row['Low']], color='black')
+            if row['Open'] < row['Close']:
+                plt.plot([i, i], [row['Open'], row['Close']], color='green')
+            else:
+                plt.plot([i, i], [row['Open'], row['Close']], color='red')
+
         plt.title(f"{symbol} Stock Price")
         plt.xlabel("Date")
         plt.ylabel("Price")
-        plt.legend()
+
+        # Customize x-axis ticks to show day/month/year format with month in all capital letters and extra space between components
+        x_ticks = [date.strftime("%d-%b-%Y").upper() for date in data.index]
+        plt.xticks(ticks=range(len(x_ticks)), labels=x_ticks)
+
         plt.show()
 
     except Exception as e:
         print(f"An error occurred while plotting {symbol} data: {e}")
+
 
 def get_next_run_time():
     now = datetime.now(pytz.timezone('US/Eastern'))
@@ -96,8 +105,8 @@ def main():
         next_run_time = get_next_run_time()
         print(f"Next run time is:", next_run_time.astimezone(pytz.timezone('US/Eastern')))
         
-        #if 1 == 1:     #debug code line to run main loop 24 hours
-        if now >= next_run_time:
+        if 1 == 1:     #debug code line to run main loop 24 hours
+        #if now >= next_run_time:
             with open('buy_signals.txt', 'w') as file:
                 file.write('')  # Clear contents of buy_signals.txt
             
@@ -105,11 +114,25 @@ def main():
                 symbols = file.read().splitlines()
 
             for symbol in symbols:
+                recommended, close_price, open_price, current_price, current_volume, average_volume, rsi, macd = analyze_stock(symbol)
+
+                print(f"\nAnalysis for {symbol}:")
+                print(f"Yesterday's Close Price: {close_price:.2f}")
+                print(f"Open Price for Today: {open_price:.2f}")
+                print(f"Current Price: {current_price:.2f}")  # Print today's current price
+                print(f"Current Volume: {current_volume:.2f}")
+                print(f"Average Volume: {average_volume:.2f}")
+                print(f"RSI: {rsi}")
+                print(f"MACD: {macd}")
+
+                plot_stock_data(symbol)
+
                 recommended, _, _, _, _, _, _, _ = analyze_stock(symbol)
                 if recommended:
                     print(f"{symbol} is recommended to buy today.")
                     subprocess.run(["espeak", f"Buy {symbol}."])
-                    plot_stock_data(symbol)
+                    with open('buy_signals.txt', 'a') as file:
+                        file.write(f"{symbol}\n")
 
             next_run_time = get_next_run_time()
             print(f"Next run time is:", next_run_time.astimezone(pytz.timezone('US/Eastern')))
