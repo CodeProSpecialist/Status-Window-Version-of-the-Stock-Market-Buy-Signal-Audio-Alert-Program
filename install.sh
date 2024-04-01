e1725d90a04bb9adc594de8396833dad53a88e96
CodeProSpecialist <111866070+CodeProSpecialist@users.noreply.github.com>
2024-03-31 19:52:47 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 # Update package lists
 apt-get update
 
 # Install system-level dependencies
-apt-get install -y python3-tk espeak
+apt-get install -y python3-tk espeak python3.10 python3-pip 
 
 # Install Python packages using pip3
-pip3 install yfinance numpy TA-Lib plotext
+pip3 install yfinance numpy TA-Lib plotext pytz 
 
 echo "Dependencies installation complete."
